c3af5073fabaa819a13b11665402baabbf29abd9
savor007 <190399720@qq.com>
2021-03-29 22:58:55 +0800

    init files for modules

--- debug.py
+++ debug.py
@@ -1,9 +1,23 @@
+import sys
 import cv2
 import numpy as np
 import dpkt
 from multiprocessing import pool
 import requests
+import pprint
+import PyQt5
+
+def TablePrint(content):
+    format_str= pprint.pformat(content, width=98)
+    pprint.pprint("|"+format_str+"|",)
+    pprint.pprint('_'*100)
+    pass 
 
 
-print("hello world")
-print("hello again")
+print(sys.stdout.name)
+TablePrint('test')
+import pip
+
+# installed_packages = pip.get_installed_distributions()
+
+print(PyQt5.__version__)
