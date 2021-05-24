caed23099572a103f2d9a7585559ccb3173903ba
savor007 <190399720@qq.com>
2021-05-24 15:15:42 +0800

    CAN replay receiver

---
+++ CAN/CANBus_RawFrames.py
@@ -0,0 +1,113 @@
+import can
+import time
+from datetime import datetime
+from pprint import pprint
+
+
+class CANBusRawFrame_Reader(object):
+    def __init__(self, can_channel, bitrate, bustype='socketcan', message_to_read=-1):
+        self.can_channel=can_channel
+        self.bitrate=bitrate
+        self.bustype=bustype
+        self._message_to_read= message_to_read
+        self._can_comm_verbose=True
+        self._can_comm_timeout=10
+        self._start_time=0
+        self._end_time=0
+        self._receive_number = 0
+
+
+    @property
+    def receive_frames(self):
+        return self._receive_number
+
+
+    @property
+    def message_to_read(self):
+        return self._message_to_read
+
+
+    @property
+    def duration(self):
+        return self._end_time - self._start_time
+
+
+    @message_to_read.setter
+    def message_to_read(self, num):
+        self._message_to_read=num
+
+
+    @property
+    def can_comm_verbose(self):
+        return self._can_comm_verbose
+
+
+    @can_comm_verbose.setter
+    def verbose(self, part_attribute):
+        self._can_comm_verbose= part_attribute
+
+
+    @property
+    def can_comm_timeout(self):
+        return self._can_comm_timeout
+
+
+    @can_comm_timeout.setter
+    def can_comm_timeout(self, timeout):
+        self._can_comm_timeout=timeout
+
+
+
+    def Start_CANChannel_RawFrames_Receive(self):
+        """
+    can_channel='can0' or 'can1'
+    """
+        try:
+            bus= can.interface.Bus(bustype=self.bustype, channel=self.can_channel, bitrate=self.bitrate, timeout=self._can_comm_timeout)
+        except Exception as error:
+            print(error)
+            return None
+        i=0
+        
+        ##for msg in bus:
+        while True:
+            try:
+                msg=bus.recv(timeout=self._can_comm_timeout)
+                if msg is None:
+                    #self._end_time= time.clock_gettime(0)
+                    break
+                elif i == 0:
+                    self._start_time=time.clock_gettime(0)
+
+            except Exception as error:
+                print(error)
+                break
+            if self._can_comm_verbose == False:
+                print("Identifier:{:4d}, Timestamp: {}, DataLength: {:2d}, Raw_Frame_Data: {}, is_FD: {}, Message_Type: {}, Error_Frame: {}, Extended_id:{}, Error_State_Indicator:{}./n".format(
+                            msg.arbitration_id, datetime.fromtimestamp(msg.timestamp), msg.dlc, msg.data, msg.is_fd, msg.id_type, msg.is_error_frame, msg.is_extended_id,msg.error_state_indicator
+                        )
+                    )
+            else:
+                print("Identifier:{:4d}, Timestamp: {}, DataLength: {:2d}, Raw_Frame_Data: {}, is_FD: {}.".format(
+                            msg.arbitration_id, datetime.fromtimestamp(msg.timestamp), msg.dlc, msg.data, msg.is_fd
+                        )
+                    )
+            i+=1
+            self._end_time= time.clock_gettime(0)
+            self._receive_number=i
+            if i>self._message_to_read & self._message_to_read>0:
+                break
+        return None
+    
+
+
+
+
+if __name__ == "__main__":
+    #can_receiver('can0', 500000, 100)
+    can_receiver=CANBusRawFrame_Reader('can0', 500000)
+    can_receiver.message_to_read=-1
+    can_receiver.can_comm_timeout=25
+    can_receiver.Start_CANChannel_RawFrames_Receive()
+    #print(can_receiver.duration)
+    print("logging duration time:{} seconds, total received frames:{}.".format(can_receiver.duration, can_receiver.receive_frames))
--- debug.py
+++ debug.py
@@ -1,11 +1,15 @@
 import sys
-import cv2
-import numpy as np
+#import cv2
+#import numpy as np
 import dpkt
 from multiprocessing import pool
 import requests
 import pprint
 import PyQt5
+import redis
+import boto3, botocore
+import time
+
 
 def TablePrint(content):
     format_str= pprint.pformat(content, width=98)
@@ -14,10 +18,33 @@ def TablePrint(content):
     pass 
 
 
-print(sys.stdout.name)
-TablePrint('test')
-import pip
+#print(sys.stdout.name)
+#TablePrint('test')
 
 # installed_packages = pip.get_installed_distributions()
 
-print(PyQt5.__version__)
+#print(PyQt5.__version__)
+# try:
+#     redis_op=redis.StrictRedis(host='10.144.130.30', port=6379,db=0)
+#     result=redis_op.setex(name='test1',time=60, value="NIIES_APAC_trans")
+#     result=redis_op.get('test1')
+#     print(result.decode('utf-8'))
+# except Exception as error:
+#     print(error)
+
+
+# try:
+#     s3=boto3.resource('s3',aws_access_key_id='AKIASW7332IZ4M4GPJXC', aws_secret_access_key='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')
+#     bucket=s3.Bucket('niiestrans')
+# except Exception as error:
+#     print(error)
+
+
+print("Identifier: {}".format(int("19a", 16)))
+
+t1=time.clock_gettime(0)
+time.sleep(2)
+print(time.clock_gettime(0)-t1)
+
+
+
