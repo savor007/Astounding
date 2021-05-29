9a47b4d52a7203d70ebbe51911a636351be7f548
savor007 <190399720@qq.com>
2021-05-29 16:34:08 +0800

    grpc command with python client support

---
+++ gRPC/gRPC_PEF_PubishCommand.py
@@ -0,0 +1,26 @@
+import grpc, datastream_pb2, datastream_pb2_grpc
+
+
+
+def invoke_publish_msg(subcriber, msg_topic,msg_body):
+    message= datastream_pb2.TopicData()
+    message.topicName=msg_topic
+    message.messageData=msg_body.encode()
+    try:
+        subcriber.PublishMessage(message)
+        print("send message successfully")
+    except Exception as error:
+        print(error)
+
+
+
+def SendMessage(message_topic="python_test_command",message_body=""):
+    with grpc.insecure_channel('169.254.115.15:9889') as channel:
+        Message_Dispatcher=datastream_pb2_grpc.DataStreamServiceStub(channel)
+        invoke_publish_msg(Message_Dispatcher, message_topic, message_body)
+
+
+if __name__ == "__main__":
+    SendMessage("PEFCommand","Resume")
+    pass
+
--- gRPC/my_grpc_server.py
+++ gRPC/my_grpc_server.py
@@ -9,7 +9,8 @@ import time
 class gRPC_Server(datastream_pb2_grpc.DataStreamServiceServicer):
     def __init__(self):
         self.__token__=list()
-        self.subscriber_mapping=dict()
+        self.subscriber_mapping={"tokenholder":{},}
+        self._terminate=dict()
 
 
 
@@ -17,12 +18,14 @@ class gRPC_Server(datastream_pb2_grpc.DataStreamServiceServicer):
         #AddSubscriber
         self.__token__.append(random.randint(2086083600,2147483640))
         print("this is a request from AddSubscriber")
+        
         return datastream_pb2.SubscriberToken(subscriberToken=self.__token__[0])
         pass
 
 
     def RemoveSubscriber(self, request, context):
         print("remove a subscriber"+str(request.subscriberToken))
+        self._terminate[request.subscriberToken]=True
         del self.subscriber_mapping[request.subscriberToken]
         return datastream_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
         pass
@@ -32,14 +35,16 @@ class gRPC_Server(datastream_pb2_grpc.DataStreamServiceServicer):
         #print(request)
         token=request.subscriberToken.subscriberToken
         topic=request.topicName
-    
-        self.subscriber_mapping[token]={topic:['test','Resume', 'Pause']} 
+        self._terminate[token]=False
+        #self.subscriber_mapping[token]={topic:['test','Resume', 'Pause']} 
+        self.subscriber_mapping[token]=dict()
         print(self.subscriber_mapping)
         return datastream_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
         pass
 
 
     def UnsubscribeTopic(self, request, context):
+        print("get a UnSubscriberdTopic request.")
         pass
 
 
@@ -48,11 +53,28 @@ class gRPC_Server(datastream_pb2_grpc.DataStreamServiceServicer):
         token=request.subscriberToken
         topic_data=self.subscriber_mapping[token]
         print(topic_data)
-        for topic, messages in topic_data.items():
-            for message in messages:
-                print("gRPC server send a message. topic:{}, message:{}.".format(topic, message))
-                time.sleep(0.18)
-                yield datastream_pb2.TopicData(topicName=topic, messageData=message.encode())
+        while True:
+            """
+            will break when receiving a remove subscriber request
+            """
+            if self._terminate[token] == True:
+                break
+            time.sleep(2)
+            yield datastream_pb2.TopicData(topicName="heart_beating", messageData="This is a stream service, so I have to keep the service yield message.".encode())
+            #####method1 send a command by pushlish grpc message, sent by subscriber##############
+            topic_data=self.subscriber_mapping[token]
+            for topic, messages in topic_data.items():
+                for message in messages:
+                    print("gRPC server send a message. topic:{}, message:{}.".format(topic, message))
+                    yield datastream_pb2.TopicData(topicName=topic, messageData=message.encode())
+                    time.sleep(0.5)
+                ##############after send all of message, clear up the message list in that topic    
+                self.subscriber_mapping[token][topic]=list()
+
+            #####method2 check Messages in Redis[remote control pef resume/pause]######
+            #####method3 Check Messages in AWS SQS[remote control pef resume/pause]#######
+
+
                 #topic_data= datastream_pb2.TopicData()
                 # if self.message_queue.empty() == False:
                 #     message_send="free_run"
@@ -60,7 +82,7 @@ class gRPC_Server(datastream_pb2_grpc.DataStreamServiceServicer):
                 #     message_send=self.message_queue.get_nowait()
                 #     self.message_queue.task_done()
                 # yield datastream_pb2.TopicData(topicName=topic, messageData=message_send.encode())
-
+        return datastream_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
 
     def StopReadingTopicMessages(self, request, context):
         # return datastream_pb2.TopicData(topicName=, messageData=)
@@ -70,18 +92,39 @@ class gRPC_Server(datastream_pb2_grpc.DataStreamServiceServicer):
 
 
     def ReadLatestTopicMessage(self, request, context):
+        print("get a ReadLatestTopic request.")
         pass
 
 
     def GetAllSubscribedTopics(self, request, context):
+        print("get a GetAllSubscriberdTopic request.")
         pass
 
 
     def GetSubscribedTopics(self, request, context):
+        print("get a GetSubscriberdTopic request.")
         pass
 
 
     def PublishMessage(self, request, context):
+        print("get a PublishMessage request.")
+        topic= request.topicName
+        msg=request.messageData.decode()
+        print("received message. topic:{}, message body:{}".format(topic, msg))
+        #if self.subscriber_mapping
+        for commands in self.subscriber_mapping.values():
+            #print(commands)
+            try:
+                message_list = commands.get(topic)
+                if message_list is None or len(message_list) == 0:
+                    commands[topic]=[msg]
+                else:
+                    message_list.append(msg)
+                    commands[topic]=message_list
+            except Exception as error:
+                print(error)
+        
+        print(self.subscriber_mapping)
         return datastream_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
         pass
 
@@ -103,5 +146,4 @@ def gRPC_Server_func():
 
 if __name__ == "__main__":
     gRPC_Server_func()
-    pass
     pass
